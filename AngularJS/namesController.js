0ff16d2cb752ee350c3b4b43daa2129f7b342824
EunjiEmmaKim <eunjieunji007@gmail.com>
2016-07-20 20:32:01 +1200

    AngularJS exercise

--- AngularJS/namesController.js
+++ AngularJS/namesController.js
@@ -1,7 +1,13 @@
 angular.module('myApp', []).controller('namesCtrl', function($scope) {
     $scope.names = [
         {name:'Jani',country:'Norway'},
-        {name:'Hege',country:'Sweden'},
-        {name:'Kai',country:'Denmark'}
+        {name:'Carl',country:'Sweden'},
+        {name:'Margareth',country:'England'},
+        {name:'Hege',country:'Norway'},
+        {name:'Joe',country:'Denmark'},
+        {name:'Gustav',country:'Sweden'},
+        {name:'Birgit',country:'Denmark'},
+        {name:'Mary',country:'England'},
+        {name:'Kai',country:'Norway'}
     ];
 });
